ff90b9e6b38d195ffea93cada824cc175381d931
chrisfaux95 <chrisfaux95@gmail.com>
2021-02-24 14:45:19 -0500

    cleaned up excess code

--- 06/main.py
+++ 06/main.py
@@ -12,26 +12,12 @@ def p1(inp):
     print("PART 1:", sum(k))
 
 def p2(inp):
-    # WORKS ON EXAMPLE INPUT
-    # DOESN'T WORK ON ACTUAL INPUT
     l = [x.split("\n") for x in inp]
     k = [[set(n) for n in x] for x in l]
     j = [len(set.intersection(*x)) for x in k]
     # print(j)
     print("PART 2:", sum(j))
 
-def p2_2(inp):
-    # print(inp)
-    l = [x.split("\n") for x in inp]
-    # print(l)
-    t_ans = []
-    for g in l:
-        # print(g)
-        shared_ans = set(g[0])
-        for p in g:
-            shared_ans &= set(p)
-        t_ans.append(len(shared_ans))
-    print("PART 2:", sum(t_ans))
     
 
 PUZZLE_INPUT = readfile(INPUT_FILE)
